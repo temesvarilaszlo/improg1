b392b127cb8ff91473046928c800879f6734f7e0
temesvarilaszlo <113339488+temesvarilaszlo@users.noreply.github.com>
2022-09-12 10:23:58 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int n = 42;
+    unsigned int ui = 10;
+    float f = 3.14;
+    double d = 3.141592;
+    char c = 'a';
+    printf("%i, %u, %f, %f, %c\n", n, ui, f, d, c);
+}
